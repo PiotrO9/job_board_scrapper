5b15c804a01fbac7de17953f1817ae21effd70b1
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-03-03 16:03:59 +0100

    Fix issue with bad element scrapping path

--- utils/ScraperWithFilter.js
+++ utils/ScraperWithFilter.js
@@ -16,19 +16,24 @@ async function ScraperWithFilter(url) {
     let Offers = await page.$$('body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a.posting-list-item div.posting-info');
     
 
+    let detailsScrappingParagraphsNumbers = [];
+    for(let i=1; i<=100; i++){
+        detailsScrappingParagraphsNumbers.push(i*2-1);
+    }
+
     for(let i = 1; i <= Offers.length; i++) {
-        let SingleSalary = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader >  nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${i}).posting-list-item div.posting-info nfj-posting-item-tags span`, element => element.innerText)
-        let SingleJobOfferName = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${i}).posting-list-item nfj-posting-item-title .posting-title__wrapper div h3`, element => element.innerText)
-        let SingleCompanyName = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${i}).posting-list-item nfj-posting-item-title .posting-title__wrapper .posting-title__company`, element => element.innerText)
+        let SingleSalary = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader >  nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${detailsScrappingParagraphsNumbers[i-1]}).posting-list-item div.posting-info nfj-posting-item-tags span`, element => element.innerText)
+        let SingleJobOfferName = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${detailsScrappingParagraphsNumbers[i-1]}).posting-list-item nfj-posting-item-title .posting-title__wrapper div h3`, element => element.innerText)
+        let SingleCompanyName = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${detailsScrappingParagraphsNumbers[i-1]}).posting-list-item nfj-posting-item-title .posting-title__wrapper .posting-title__company`, element => element.innerText)
         let LogoUrl; 
         try {
-            LogoUrl = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${i}).posting-list-item .posting-image common-image-blur div picture img`, element => element.src)
+            LogoUrl = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${detailsScrappingParagraphsNumbers[i-1]}).posting-list-item .posting-image common-image-blur div picture img`, element => element.src)
         }
         catch(error) {
             LogoUrl = undefined
         }
-        let JobCity = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${i}).posting-list-item div.posting-info nfj-posting-item-city div span`, element => element.innerText)
-        let JobDetailsLink = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${i}).posting-list-item`, element => element.href)
+        let JobCity = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${detailsScrappingParagraphsNumbers[i-1]}).posting-list-item div.posting-info nfj-posting-item-city div span`, element => element.innerText)
+        let JobDetailsLink = await page.$eval(`body > nfj-root > nfj-layout > nfj-main-content > div > nfj-postings-search > div > common-main-loader > nfj-search-results > nfj-postings-list:nth-child(1) > div.list-container.ng-star-inserted a:nth-child(${detailsScrappingParagraphsNumbers[i-1]}).posting-list-item`, element => element.href)
 
         let offer = {
             salary: SingleSalary,
